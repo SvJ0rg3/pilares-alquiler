b81c4c094618fd45306802ac736baf26ef35bb13
Jorginho <jorgeluisalcedovega@gmail.com>
2026-09-17 14:59:47 -0500

    jorge salcedo

--- src/Clases/Carro.java
+++ src/Clases/Carro.java
@@ -1,16 +1,1 @@
-package Clases;
 
-public class Carro extends Vehiculo {
-
-    public Carro(String marca, String modelo, double tarifaBase) {
-        super(marca, modelo, tarifaBase);
-    }
-
-    @Override
-    public double calcularCostoDeAlquiler(int dias) {
-        double costoBase = tarifaBase*dias;
-        double costoExtra = dias * 10;
-
-        return costoBase + costoExtra;
-    }
-}
